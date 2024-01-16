51343c2fbd80711aae9734abb3e194c1979f9d5e
Miles <81843550+Y2Kwastaken@users.noreply.github.com>
2024-01-16 20:46:36 +0000

    cloud framework (#2)

--- core/src/main/kotlin/com/github/spigotbasics/core/module/AbstractBasicsModule.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/module/AbstractBasicsModule.kt
@@ -1,11 +1,22 @@
 package com.github.spigotbasics.core.module
 
+import cloud.commandframework.SenderMapper
+import cloud.commandframework.bukkit.BukkitCommandManager
+import cloud.commandframework.execution.ExecutionCoordinator
 import com.github.spigotbasics.core.BasicsPlugin
-import java.io.InputStream
 import java.net.URL
 import java.util.logging.Logger
 
-abstract class AbstractBasicsModule(protected val plugin: BasicsPlugin, final override val info: ModuleInfo) : BasicsModule {
+abstract class AbstractBasicsModule(protected val plugin: BasicsPlugin, final override val info: ModuleInfo) :
+    BasicsModule {
+
+    init {
+        val commandManager = BukkitCommandManager(
+            plugin,
+            ExecutionCoordinator.simpleCoordinator(),
+            SenderMapper.identity()
+        );
+    }
 
     override val logger: Logger = Logger.getLogger(info.mainClass)
 
@@ -15,12 +26,11 @@ abstract class AbstractBasicsModule(protected val plugin: BasicsPlugin, final ov
         return javaClass.getResource(path) ?: error("Resource $path not found")
     }
 
-    override fun enable() { }
+    override fun enable() {}
 
-    override fun disable() { }
+    override fun disable() {}
 
-    override fun load() { }
-
+    override fun load() {}
 
 
 }
