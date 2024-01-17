fec2febcbd9b88f05dee2081c3b1ea89176dcbdc
Miles Holder <mwholder2005@gmail.com>
2024-01-17 15:05:52 -0600

    remove companion object

--- core/src/main/kotlin/com/github/spigotbasics/core/config/SavedConfig.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/config/SavedConfig.kt
@@ -2,8 +2,6 @@ package com.github.spigotbasics.core.config
 
 import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
 
 class SavedConfig(private val saveFile: File) : YamlConfiguration() {
 
@@ -11,18 +9,4 @@ class SavedConfig(private val saveFile: File) : YamlConfiguration() {
         save(saveFile)
     }
 
-    companion object {
-        fun saveAndLoadDefaults(saveFile: File, resourceStream: InputStream): SavedConfig {
-            val configuration = SavedConfig(saveFile);
-            configuration.setDefaults(YamlConfiguration.loadConfiguration(saveFile));
-
-            if (!saveFile.exists()) {
-                saveFile.createNewFile();
-                resourceStream.copyTo(FileOutputStream(saveFile))
-            }
-
-            configuration.load(saveFile)
-            return configuration;
-        }
-    }
 }
