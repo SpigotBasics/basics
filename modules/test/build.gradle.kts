885e7854e1c8734716fe3875512a5bdf3aa4a111
mfnalex <mfnalex@jeff-media.com>
2024-01-17 01:53:48 +0100

    added shaded dummy dependency

--- modules/test/build.gradle.kts
+++ modules/test/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     id("basics.module")
 }
+
+dependencies {
+    // Test dependency - This should get shaded without further configuration.
+    // https://mvnrepository.com/artifact/org.yaml/snakeyaml
+    implementation("org.yaml:snakeyaml:2.2")
+
+}
