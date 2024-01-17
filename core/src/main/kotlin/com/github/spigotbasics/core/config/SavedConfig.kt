8b96a007a85c00de936aa7cb2d23d36025387b24
mfnalex <mfnalex@jeff-media.com>
2024-01-17 22:26:07 +0100

    added javadocs and static load method

--- core/src/main/kotlin/com/github/spigotbasics/core/config/SavedConfig.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/config/SavedConfig.kt
@@ -3,10 +3,30 @@ package com.github.spigotbasics.core.config
 import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
 
-class SavedConfig(private val saveFile: File) : YamlConfiguration() {
+/**
+ * Represents a [YamlConfiguration] that is backed by a file.
+ *
+ * @property file File backing this configuration
+ * @constructor Create empty Saved config
+ */
+class SavedConfig(private val file: File) : YamlConfiguration() {
+
+    companion object {
+        /**
+         * Creates a new [SavedConfig] from the given file.
+         *
+         * @param file File to load from
+         * @return New [SavedConfig] instance
+         */
+        fun load(file: File): SavedConfig {
+            val config = SavedConfig(file)
+            config.load(file)
+            return config
+        }
+    }
 
     fun save() {
-        save(saveFile)
+        save(file)
     }
 
 }
