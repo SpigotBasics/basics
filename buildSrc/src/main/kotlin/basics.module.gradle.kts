333fbc48968b9d192e17245f45ce9160d458ce68
mfnalex <mfnalex@jeff-media.com>
2024-01-17 01:53:36 +0100

    added shadow configu for modules

--- buildSrc/src/main/kotlin/basics.module.gradle.kts
+++ buildSrc/src/main/kotlin/basics.module.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 /**
  * Used for all modules
  */
@@ -6,8 +8,23 @@ plugins {
     id("basics.kotlin-conventions")
     id("basics.dependency.spigot-api")
     `java-library`
+    id("com.github.johnrengelman.shadow")
 }
+
+apply(plugin = "com.github.johnrengelman.shadow")
 
 dependencies {
     compileOnly(project(":core"))
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.getByName("shadowJar", ShadowJar::class).apply {
+    archiveClassifier.set("shaded")
+    dependencies {
+        exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
+        exclude(dependency("org.jetbrains:annotations"))
+    }
 }
