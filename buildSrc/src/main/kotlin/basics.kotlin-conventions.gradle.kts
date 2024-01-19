40483aecf97795015e54110ff36e2a11034fe17a
mfnalex <mfnalex@jeff-media.com>
2024-01-19 18:57:31 +0100

    moved getGroupId to separate file

--- buildSrc/src/main/kotlin/basics.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/basics.kotlin-conventions.gradle.kts
@@ -1,6 +1,10 @@
 /**
  * Base plugin for Kotlin
  */
+
+group = getGroupId()
+version = "1.0-SNAPSHOT"
+
 plugins {
     kotlin("jvm")
 }
@@ -16,12 +20,6 @@ repositories {
         }
     }
 
-// This was only required for cloud
-//    maven {
-//        name = "sonatypeOssSnapshots"
-//        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-//    }
-
     maven {
         name = "aikar"
         url = uri("https://repo.aikar.co/content/groups/aikar/")
@@ -31,25 +29,7 @@ repositories {
     }
 }
 
-/**
- * Returns a proper group ID for this subproject, e.g. "com.github.spigotbasics.modules" for modules/tpa
- * TODO: This should be moved into the main build script into allprojects
- */
-fun getGroupId(): String {
-    val groupId = "com.github.spigotbasics"
-    val myParentDir = projectDir.parentFile
-    if (rootDir == myParentDir) {
-        return groupId
-    }
-    val relativeGroupId = myParentDir.relativeTo(rootDir).path.replace(File.separatorChar, '.')
-    if (relativeGroupId.isEmpty()) {
-        throw RuntimeException("Couldn't determine relative group ID for $projectDir")
-    }
-    return "${groupId}.${relativeGroupId}"
-}
 
-group = getGroupId()
-version = "1.0-SNAPSHOT"
 
 kotlin {
     jvmToolchain(8)
