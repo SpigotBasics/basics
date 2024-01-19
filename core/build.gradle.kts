4db12941be6686d254781d5029613d25dee760a5
mfnalex <mfnalex@jeff-media.com>
2024-01-19 18:42:21 +0100

    moved ACF dependency to own dependency script, moved all repos to kotlin-conventions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,12 +3,11 @@ import org.jetbrains.dokka.gradle.DokkaTask
 plugins {
     id("basics.kotlin-conventions")
     id("basics.dependency.spigot-api")
+    id("basics.dependency.acf")
     id("org.jetbrains.dokka") version "1.9.10"
 }
 
 dependencies {
-    api("co.aikar:acf-paper:0.5.1-SNAPSHOT")
-    //implementation(kotlin("reflect"))
     api("net.kyori:adventure-api:4.14.0")
     api("net.kyori:adventure-platform-bukkit:4.3.1")
 }
