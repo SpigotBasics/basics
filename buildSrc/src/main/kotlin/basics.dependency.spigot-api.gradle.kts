a36baf36903b684746243bc05515089b8d3dc266
mfnalex <mfnalex@jeff-media.com>
2024-01-19 18:42:21 +0100

    moved ACF dependency to own dependency script, moved all repos to kotlin-conventions

--- buildSrc/src/main/kotlin/basics.dependency.spigot-api.gradle.kts
+++ buildSrc/src/main/kotlin/basics.dependency.spigot-api.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     `java-library`
 }
 
-repositories {
-    maven {
-        name = "spigotmc"
-        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    }
-}
-
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
 }
