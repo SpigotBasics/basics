5dbd78b07c5d35447a9950fe88b12c89e6b06b06
Miles <81843550+Y2Kwastaken@users.noreply.github.com>
2024-01-21 19:17:06 +0000

    command manager (#11)
    remove refreshCommands
    
    comment unused field

--- core/src/main/kotlin/com/github/spigotbasics/core/BasicsPlugin.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/BasicsPlugin.kt
@@ -1,17 +1,15 @@
 package com.github.spigotbasics.core
 
-import co.aikar.commands.PaperCommandManager
-import com.github.spigotbasics.core.module.BasicsModule
+import com.github.spigotbasics.core.command.BasicsCommandManager
 import com.github.spigotbasics.core.module.manager.ModuleManager
 import org.bukkit.plugin.Plugin
 import java.io.File
-import java.util.logging.Logger
-import kotlin.reflect.KClass
 
 interface BasicsPlugin: Plugin {
 
     //val logger: Logger // Already in Bukkit:Plugin
     val moduleFolder: File
     val moduleManager: ModuleManager
-    val commandManager: PaperCommandManager
+
+    fun createCommandManager(): BasicsCommandManager
 }
