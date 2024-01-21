dde5802d43d9f857521dd326b62da0f3454be815
mfnalex <mfnalex@jeff-media.com>
2024-01-21 15:22:55 +0100

    prevent access to forbidden classes

--- core/src/main/kotlin/com/github/spigotbasics/core/module/loader/ModuleJarClassLoader.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/module/loader/ModuleJarClassLoader.kt
@@ -1,9 +1,22 @@
 package com.github.spigotbasics.core.module.loader
 
+import com.github.spigotbasics.core.module.ForbiddenFruitException
+import org.bukkit.scheduler.BukkitScheduler
 import java.io.File
-import java.net.URL
 import java.net.URLClassLoader
 
-class ModuleJarClassLoader(val file: File, val parentLoader: ClassLoader) : URLClassLoader(arrayOf( file.toURI().toURL()), parentLoader) {
+class ModuleJarClassLoader(val file: File, parentLoader: ClassLoader) :
+    URLClassLoader(arrayOf(file.toURI().toURL()), parentLoader) {
 
+    companion object {
+        val FORBIDDEN_CLASSES = listOf(BukkitScheduler::class.java).map { it.name }
+    }
+
+    override fun loadClass(name: String, resolve: Boolean): Class<*> {
+        //println("Loading class $name (resolve: $resolve)")
+        if (FORBIDDEN_CLASSES.contains(name)) {
+            throw ForbiddenFruitException(name)
+        }
+        return super.loadClass(name, resolve)
+    }
 }
