6d3a6d012e84597bf49e10fbf8be7b10deb497fb
mfnalex <mfnalex@jeff-media.com>
2024-01-21 20:58:18 +0100

    Join/Leave messages module

---
+++ modules/join-messages/src/main/kotlin/com/github/spigotbasics/modules/joinmessages/JoinMessagesModule.kt
@@ -0,0 +1,31 @@
+package com.github.spigotbasics.modules.joinmessages
+
+import com.github.spigotbasics.core.module.AbstractBasicsModule
+import com.github.spigotbasics.core.module.ModuleInstantiationContext
+import com.github.spigotbasics.core.replace.mini
+import com.github.spigotbasics.core.replace.placeholders
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+
+class JoinMessagesModule(context: ModuleInstantiationContext) : AbstractBasicsModule(context), Listener {
+
+    override fun onEnable() {
+        plugin.server.pluginManager.registerEvents(this, plugin)
+    }
+
+    private fun getJoinMessage(): String? = config.getString("join-message")
+    private fun getQuitMessage(): String? = config.getString("quit-message")
+
+    @EventHandler
+    fun joinMessage(event: PlayerJoinEvent) {
+        event.joinMessage = getJoinMessage()?.placeholders(event.player)?.mini() ?: return
+    }
+
+    @EventHandler
+    fun leaveMessage(event: PlayerQuitEvent) {
+        event.quitMessage = getQuitMessage()?.placeholders(event.player)?.mini() ?: return
+    }
+    
+}
