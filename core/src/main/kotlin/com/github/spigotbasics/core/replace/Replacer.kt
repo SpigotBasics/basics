95ed664e54ec2c7cc5a054a973720602b4cd65e4
mfnalex <mfnalex@jeff-media.com>
2024-01-21 21:28:13 +0100

    revamped replacement functions, added papi replacement function

--- core/src/main/kotlin/com/github/spigotbasics/core/replace/Replacer.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/replace/Replacer.kt
@@ -1,55 +1,105 @@
 package com.github.spigotbasics.core.replace
 
+import me.clip.placeholderapi.PlaceholderAPI
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import org.bukkit.Bukkit
+import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 
 val mini: MiniMessage = MiniMessage.builder().strict(false).build()
 val legacy: LegacyComponentSerializer = LegacyComponentSerializer.builder().hexColors().useUnusualXRepeatedCharacterHexFormat().build()
 
-fun String.placeholders(vararg placeholders: Pair<*, *>): String {
+fun hasPapi(): Boolean {
+    return Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")
+}
+
+/**
+ * Applies the given placeholders to the string. Placeholders are in the format %placeholder_name%.
+ *
+ * @param placeholders The placeholders to apply
+ * @return The string with the placeholders applied
+ */
+fun String.placeholders(placeholders: Map<String, String>): String {
     var result = this
+
     placeholders.forEach { (placeholder, replacement) ->
-        val placeholderStr = placeholder.toString()
-        val replacementStr = replacement.toString()
-        result = result.replace("%${placeholderStr}%", replacementStr)
+        result = result.replace("%${placeholder}%", replacement)
     }
     return result
 }
 
-fun String.placeholders(player: Player): String {
-    return this.placeholders(
+
+/**
+ * Applies basic player Placeholders to the string. This is used as a fallback if PlaceholderAPI is not installed.
+ * The following placeholders are available:
+ * - player_name
+ * - player_uuid
+ * - player_world
+ * - player_x
+ * - player_y
+ * - player_z
+ * - player_displayname
+ *
+ * @param player The player to apply the placeholders for
+ * @return The string with the placeholders applied
+ */
+fun String.papiFallbackPlayer(player: Player): String {
+    return this.placeholders(mapOf(
         "player_name" to player.name,
         "player_uuid" to player.uniqueId.toString(),
         "player_world" to player.world.name,
         "player_x" to player.location.x.toString(),
         "player_y" to player.location.y.toString(),
         "player_z" to player.location.z.toString(),
-        "player_display_name" to player.displayName,
-    )
+        "player_displayname" to player.displayName,
+    ))
 }
 
-fun String.placeholders(vararg replacements: Any): String {
-    var result = this
-    replacements.forEach { arg: Any ->
-        if(arg is Pair<*,*>) {
-            result = result.placeholders(arg)
-        } else if(arg is Player) {
-            result = result.placeholders(arg)
+
+/**
+ * Applies PlaceholderAPI. If PlaceholderAPI is not installed, but an online player is given, it falls back to replacing
+ * some generic placeholders. See [String.papiFallbackPlayer] for more information.
+ *
+ * @param player The player to apply the placeholders for
+ * @return The string with the placeholders applied
+ */
+fun String.papi(player: OfflinePlayer? = null): String {
+    if(hasPapi()) {
+        return PlaceholderAPI.setPlaceholders(player, this)
+    } else {
+        if(player is Player) {
+            return this.papiFallbackPlayer(player)
+        } else {
+            return this
         }
     }
-    return result
 }
 
+/**
+ * Parses MiniMessage into a Component
+ *
+ * @return The parsed component
+ */
 fun String.miniComponents(): Component {
     return mini.deserialize(this)
 }
 
+/**
+ * Parses MiniMessage into a Bukkit String. This is the same as calling [String.miniComponents] and then [Component.toLegacy]
+ *
+ * @return The parsed string
+ */
 fun String.mini(): String {
     return this.miniComponents().toLegacy()
 }
 
+/**
+ * Parses a Component into a Bukkit String
+ *
+ * @return The parsed string
+ */
 fun Component.toLegacy(): String {
     return legacy.serialize(this)
 }
