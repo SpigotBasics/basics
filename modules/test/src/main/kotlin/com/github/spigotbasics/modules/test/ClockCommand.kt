096d63eecfc1a79cf1868568f4e57e56c23b77de
mfnalex <mfnalex@jeff-media.com>
2024-01-21 15:23:40 +0100

    added test command to check access to forbidden classes

--- modules/test/src/main/kotlin/com/github/spigotbasics/modules/test/ClockCommand.kt
+++ modules/test/src/main/kotlin/com/github/spigotbasics/modules/test/ClockCommand.kt
@@ -3,9 +3,11 @@ package com.github.spigotbasics.modules.test
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.Default
+import co.aikar.commands.annotation.Subcommand
+import com.github.spigotbasics.core.module.BasicsModule
+import com.github.spigotbasics.core.module.ForbiddenFruitException
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
-import org.bukkit.plugin.Plugin
 
 @CommandAlias("clock")
 class ClockCommand(private val module: BasicsModule) : BaseCommand() {
@@ -17,4 +19,14 @@ class ClockCommand(private val module: BasicsModule) : BaseCommand() {
         }
     }
 
+    @Subcommand("tryscheduler")
+    fun tryScheduler() {
+        try {
+            Bukkit.getScheduler().runTaskLater(module.plugin, Runnable {
+                Bukkit.broadcastMessage("§cI was able to access the scheduler :< :< :<")
+            }, 5)
+        } catch (e: ForbiddenFruitException) {
+            Bukkit.broadcastMessage("§aI was not able to access the scheduler :3")
+        }
+    }
 }
