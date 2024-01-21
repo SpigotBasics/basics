f6496d2cfae8a1d4ab0e14bb9f78381f61c2aefe
mfnalex <mfnalex@jeff-media.com>
2024-01-21 16:28:46 +0100

    better exception message

--- core/src/main/kotlin/com/github/spigotbasics/core/module/ForbiddenFruitException.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/module/ForbiddenFruitException.kt
@@ -1,5 +1,8 @@
 package com.github.spigotbasics.core.module
 
-class ForbiddenFruitException(clazzName: String) :
-    Exception("Module tried to eat forbidden fruit $clazzName") {
+import com.github.spigotbasics.core.scheduler.BasicsScheduler
+import kotlin.reflect.KProperty1
+
+class ForbiddenFruitException(clazzName: String, replacement: KProperty1<BasicsModule, BasicsScheduler>) :
+    Exception("Module tried to access forbidden class \"$clazzName\" - use \"${replacement}\" instead") {
 }
