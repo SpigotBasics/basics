c095c4d0b572f5c262e4620808561e488607328b
mfnalex <mfnalex@jeff-media.com>
2024-01-21 15:23:12 +0100

    created CommandCompletions.kt for completion aliases

---
+++ plugin/src/main/kotlin/com/github/spigotbasics/plugin/commands/CommandCompletions.kt
@@ -0,0 +1,9 @@
+package com.github.spigotbasics.plugin.commands
+
+object CommandCompletions {
+    const val ENABLED_MODULES = "enabled_modules"
+    const val DISABLED_MODULES = "disabled_modules"
+    const val LOADED_MODULES = "loaded_modules"
+    const val ALL_MODULE_FILES = "all_module_files"
+
+}
