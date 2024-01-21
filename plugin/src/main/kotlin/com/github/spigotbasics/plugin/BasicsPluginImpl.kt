a7015905d6ccccdcd62adf72b0524e08d0736b06
mfnalex <mfnalex@jeff-media.com>
2024-01-21 15:24:30 +0100

    improved module loading & completions

--- plugin/src/main/kotlin/com/github/spigotbasics/plugin/BasicsPluginImpl.kt
+++ plugin/src/main/kotlin/com/github/spigotbasics/plugin/BasicsPluginImpl.kt
@@ -3,8 +3,10 @@ package com.github.spigotbasics.plugin
 import co.aikar.commands.PaperCommandManager
 import com.github.spigotbasics.core.BasicsLoggerFactory
 import com.github.spigotbasics.core.BasicsPlugin
+import com.github.spigotbasics.core.module.loader.ModuleJarFileFilter
 import com.github.spigotbasics.core.module.manager.ModuleManager
 import com.github.spigotbasics.plugin.commands.BasicsCommand
+import com.github.spigotbasics.plugin.commands.CommandCompletions
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 
@@ -28,7 +30,7 @@ class BasicsPluginImpl : JavaPlugin(), BasicsPlugin {
 
     override fun onEnable() {
         setupAcf()
-        moduleManager.loadModules()
+        moduleManager.loadAndEnableAllModulesFromModulesFolder()
     }
 
     private fun setupAcf() {
@@ -39,16 +41,20 @@ class BasicsPluginImpl : JavaPlugin(), BasicsPlugin {
 
     private fun registerCommandCompletions() {
         val completions = commandManager.commandCompletions
-        completions.registerAsyncCompletion("allmodules") {
-            moduleManager.modules.map { it.info.name }
+        completions.registerAsyncCompletion(CommandCompletions.LOADED_MODULES) {
+            moduleManager.loadedModules.map { it.info.name }
         }
 
-        completions.registerAsyncCompletion("enabledmodules") {
+        completions.registerAsyncCompletion(CommandCompletions.ENABLED_MODULES) {
             moduleManager.enabledModules.map { it.info.name }
         }
 
-        completions.registerAsyncCompletion("disabledmodules") {
+        completions.registerAsyncCompletion(CommandCompletions.DISABLED_MODULES) {
             moduleManager.disabledModules.map { it.info.name }
+        }
+
+        completions.registerAsyncCompletion(CommandCompletions.ALL_MODULE_FILES) {
+            moduleFolder.listFiles(ModuleJarFileFilter)?.map { it.name } ?: listOf()
         }
 
     }
