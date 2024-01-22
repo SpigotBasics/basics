3f83d25fc962d7a7529bf7a13f5dae905d3623d3
mfnalex <mfnalex@jeff-media.com>
2024-01-22 08:07:58 +0100

    added join-self message, switched to Message class

--- modules/join-messages/src/main/kotlin/com/github/spigotbasics/modules/joinmessages/JoinMessagesModule.kt
+++ modules/join-messages/src/main/kotlin/com/github/spigotbasics/modules/joinmessages/JoinMessagesModule.kt
@@ -1,9 +1,8 @@
 package com.github.spigotbasics.modules.joinmessages
 
+import com.github.spigotbasics.core.config.Message
 import com.github.spigotbasics.core.module.AbstractBasicsModule
 import com.github.spigotbasics.core.module.ModuleInstantiationContext
-import com.github.spigotbasics.core.replace.miniComponents
-import com.github.spigotbasics.core.replace.papi
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
@@ -16,21 +15,22 @@ class JoinMessagesModule(context: ModuleInstantiationContext) : AbstractBasicsMo
         eventBus.subscribe(this)
     }
 
-    private fun getJoinMessage(): String? = config.getString("join-message")
-    private fun getQuitMessage(): String? = config.getString("quit-message")
+    private fun getJoinMessage(): Message = config.getMessage("join")
+    private fun getJoinSelfMessage(): Message = config.getMessage("join-self")
+    private fun getQuitMessage(): Message = config.getMessage("quit")
 
     @EventHandler
     fun joinMessage(event: PlayerJoinEvent) {
         event.joinMessage = null
-        val component = getJoinMessage()?.papi(event.player)?.miniComponents() ?: return
-        audience.all().sendMessage(component)
+        getJoinMessage().papi(event.player).sendMiniTo(audience.filter { it != event.player })
+        getJoinSelfMessage().papi(event.player).sendMiniTo(audience.player(event.player))
+
     }
 
     @EventHandler
     fun leaveMessage(event: PlayerQuitEvent) {
         event.quitMessage = null
-        val component = getQuitMessage()?.papi(event.player)?.miniComponents() ?: return
-        audience.all().sendMessage(component)
+        getQuitMessage().papi(event.player).sendMiniTo(audience.all())
     }
 
 
