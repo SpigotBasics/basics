a1df16923e47738eee456ca55371a26ee2bec0f0
mfnalex <mfnalex@jeff-media.com>
2024-01-23 07:59:33 +0100

    flattened command variants into separate functions

--- modules/basics-repair/src/main/kotlin/com/github/spigotbasics/modules/basicsrepair/RepairCommand.kt
+++ modules/basics-repair/src/main/kotlin/com/github/spigotbasics/modules/basicsrepair/RepairCommand.kt
@@ -3,54 +3,75 @@ package com.github.spigotbasics.modules.basicsrepair
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
-import co.aikar.commands.annotation.Flags
-import co.aikar.commands.annotation.Optional
-import co.aikar.commands.annotation.Subcommand
+import co.aikar.commands.annotation.Default
+import co.aikar.commands.bukkit.contexts.OnlinePlayer
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 
-@CommandAlias("repair")
-@CommandPermission("basics.command.repair")
+@CommandAlias("repair|fix")
 class RepairCommand(private val module: BasicsRepairModule) : BaseCommand() {
 
-    @Subcommand("hand")
+    @Default
     @CommandPermission("basics.command.repair.hand")
-    fun repairHand(player: Player, @CommandPermission("basics.command.repair.hand.other") @Optional other: Player?) {
-        val target = other ?: player
-
-        repairItem(target.inventory.itemInMainHand)
-        if (target == player) {
-            module.repairHandSelf.papi(target).sendMiniTo(module.audience.player(player))
-        } else {
-            module.repairHand.papi(target).sendMiniTo(module.audience.player(player))
-        }
+    fun runHandSelf(sender: Player) {
+        repairSingle(sender, sender)
     }
 
-    @Subcommand("all")
-    @CommandPermission("basics.command.repair.all")
-    fun repairAll(player: Player, @CommandPermission("basics.command.repair.all.other") @Optional other: Player?) {
-        val target = other ?: player
+    @Default
+    @CommandPermission("basics.command.repair.hand.other")
+    fun runHandOther(sender: CommandSender, target: OnlinePlayer) {
+        repairSingle(sender, target.player)
+    }
 
+    @CommandAlias("repairall|fixall")
+    @CommandPermission("basics.command.repair.all")
+    fun runAllSelf(sender: Player) {
+        repairAll(sender, sender)
+    }
+
+    @CommandAlias("repairall|fixall")
+    @CommandPermission("basics.command.repair.all.other")
+    fun runAllOther(sender: CommandSender, target: OnlinePlayer) {
+        repairAll(sender, target.player)
+    }
+
+    fun repairSingle(sender: CommandSender, target: Player) {
+        repairItem(target.inventory.itemInMainHand)
+
+        val message = if (target == sender) {
+            module.repairHandSelf
+        } else {
+            module.repairHand
+        }
+
+        message.papi(target).sendMiniTo(module.audience.sender(sender))
+    }
+
+    fun repairAll(sender: CommandSender, target: Player) {
         for (content in target.inventory.contents) {
             repairItem(content)
         }
 
-        if (target == player) {
-            module.repairAllSelf.papi(target).sendMiniTo(module.audience.player(player))
+        val message = if (target == sender) {
+            module.repairAllSelf
         } else {
-            module.repairAll.papi(target).sendMiniTo(module.audience.player(player))
+            module.repairAll
         }
+
+        message.papi(target).sendMiniTo(module.audience.sender(sender))
     }
 
-    fun repairItem(item: ItemStack?): Boolean {
-        if (item == null) return false
-        val meta = item.itemMeta as Damageable
-        if (meta.hasDamage()) {
-            meta.damage = 0
-            item.setItemMeta(meta)
+    private fun repairItem(item: ItemStack?) {
+        if (item == null || item.type.isAir) return
+        val meta = item.itemMeta
+        if(meta is Damageable) {
+            if (meta.hasDamage()) {
+                meta.damage = 0
+                item.setItemMeta(meta)
+            }
         }
-        return true
     }
 
 }
