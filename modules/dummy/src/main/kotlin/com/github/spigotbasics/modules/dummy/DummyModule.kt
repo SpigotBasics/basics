50d0ed841ca1229ab749a270bfcf0e6fdccccd18
mfnalex <mfnalex@jeff-media.com>
2024-01-25 00:02:23 +0100

    added dummy update to test module unloading

---
+++ modules/dummy/src/main/kotlin/com/github/spigotbasics/modules/dummy/DummyModule.kt
@@ -0,0 +1,12 @@
+package com.github.spigotbasics.modules.dummy
+
+import com.github.spigotbasics.core.module.AbstractBasicsModule
+import com.github.spigotbasics.core.module.ModuleInstantiationContext
+
+class DummyModule(context: ModuleInstantiationContext) : AbstractBasicsModule(context) {
+
+    override fun onEnable() {
+    
+    }
+    
+}
