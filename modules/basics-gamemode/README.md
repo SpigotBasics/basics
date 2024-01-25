e1a0476c46f01d6c6ff1a248ce4f6a5d448b59b2
mfnalex <mfnalex@jeff-media.com>
2024-01-25 19:12:58 +0100

    added description and readme to gamemode module (#25)
    * added command descriptions
    
    * added gamemode-module readme
    
    * fixed html tags

---
+++ modules/basics-gamemode/README.md
@@ -0,0 +1,16 @@
+| Command                        | Permission                               | Description                             |
+|--------------------------------|------------------------------------------|-----------------------------------------|
+| /gamemode &lt;gamemode&gt;           | basics.command.gamemode.&lt;gamemode&gt;       | Sets your game mode to &lt;gamemode&gt;       |
+| /gamemode &lt;gamemode&gt; &lt;player&gt;  | basics.command.gamemode.&lt;gamemode&gt;.other | Sets &lt;player&gt;'s game mode to &lt;gamemode&gt; |
+
+Available game modes are `survival`, `creative`, `adventure` and `spectator`.
+
+You can also use the following aliases:
+
+`/survival` or `/gms` or `/gm 0` for `/gamemode survival`
+
+`/creative` or `/gmc` or `/gm 1` for `/gamemode creative`
+
+`/adventure` or `/gma` or `/gm 2` for `/gamemode adventure`
+
+`/spectator` or `/gmsp` or `/gm 3` for `/gamemode spectator`
--- modules/basics-gamemode/src/main/kotlin/com/github/spigotbasics/modules/basicsgamemode/GamemodeCommand.kt
+++ modules/basics-gamemode/src/main/kotlin/com/github/spigotbasics/modules/basicsgamemode/GamemodeCommand.kt
@@ -3,6 +3,7 @@ package com.github.spigotbasics.modules.basicsgamemode
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Description
 import co.aikar.commands.annotation.Subcommand
 import co.aikar.commands.bukkit.contexts.OnlinePlayer
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
@@ -12,6 +13,7 @@ import org.bukkit.entity.Player
 
 @CommandAlias("gamemode|gm")
 @CommandPermission("basics.command.gamemode")
+@Description("Change your or someone else's game mode")
 class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
 
     // SURVIVAL
@@ -19,6 +21,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("survival|s|0")
     @CommandAlias("survival|gm0|gms")
     @CommandPermission("basics.command.gamemode.survival")
+    @Description("Sets your game mode to survival")
     fun onSurvivalSelf(player: Player) {
         set(player, player, GameMode.SURVIVAL)
     }
@@ -26,6 +29,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("survival|s|0")
     @CommandAlias("survival|gm0|gms")
     @CommandPermission("basics.command.gamemode.survival.other")
+    @Description("Sets the given player's game mode to survival")
     fun onSurvivalOther(sender: CommandSender, player: OnlinePlayer) {
         set(sender, player.player, GameMode.SURVIVAL)
     }
@@ -35,6 +39,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("creative|c|1")
     @CommandAlias("creative|gm1|gmc")
     @CommandPermission("basics.command.gamemode.creative")
+    @Description("Sets your game mode to creative")
     fun onCreativeSelf(player: Player) {
         set(player, player, GameMode.CREATIVE)
     }
@@ -42,6 +47,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("creative|c|1")
     @CommandAlias("creative|gm1|gmc")
     @CommandPermission("basics.command.gamemode.creative.other")
+    @Description("Sets the given player's game mode to creative")
     fun onCreativeOther(sender: CommandSender, player: OnlinePlayer) {
         set(sender, player.player, GameMode.CREATIVE)
     }
@@ -51,6 +57,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("adventure|a|2")
     @CommandAlias("adventure|gm2|gma")
     @CommandPermission("basics.command.gamemode.adventure")
+    @Description("Sets your game mode to adventure")
     fun onAdventureSelf(player: Player) {
         set(player, player, GameMode.ADVENTURE)
     }
@@ -58,6 +65,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("adventure|a|2")
     @CommandAlias("adventure|gm2|gma")
     @CommandPermission("basics.command.gamemode.adventure.other")
+    @Description("Sets the given player's game mode to adventure")
     fun onAdventureOther(sender: CommandSender, player: OnlinePlayer) {
         set(sender, player.player, GameMode.ADVENTURE)
     }
@@ -66,6 +74,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("spectator|sp|3")
     @CommandAlias("spectator|gm3|gmsp")
     @CommandPermission("basics.command.gamemode.spectator")
+    @Description("Sets your game mode to spectator")
     fun onSpectatorSelf(player: Player) {
         set(player, player, GameMode.SPECTATOR)
     }
@@ -73,6 +82,7 @@ class GamemodeCommand(val module: BasicsGamemodeModule) : BaseCommand() {
     @Subcommand("spectator|sp|3")
     @CommandAlias("spectator|gm3|gmsp")
     @CommandPermission("basics.command.gamemode.spectator.other")
+    @Description("Sets the given player's game mode to spectator")
     fun onSpectatorOther(sender: CommandSender, player: OnlinePlayer) {
         set(sender, player.player, GameMode.SPECTATOR)
     }
