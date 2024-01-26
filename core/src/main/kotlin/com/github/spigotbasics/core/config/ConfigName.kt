17fbeb51c6b823e13b19971fe1b0d21507e8cdb3
mfnalex <mfnalex@jeff-media.com>
2024-01-26 23:38:39 +0100

    fixed core messages.yml being used instead of module messages.yml

--- core/src/main/kotlin/com/github/spigotbasics/core/config/ConfigName.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/config/ConfigName.kt
@@ -21,7 +21,7 @@ class ConfigName private constructor(val path: String){
         /**
          * Messages config name
          */
-        val MESSAGES = ConfigName("messages.yml")
+        val MESSAGES = ConfigName("core-messages.yml")
 
 
         fun fromName(name: String): ConfigName {
@@ -30,7 +30,7 @@ class ConfigName private constructor(val path: String){
                     logger.warning("Module tried to get default config using name $name instead of ConfigName.CONFIG")
                     CONFIG
                 }
-                "messages", "messages.yml" -> {
+                "messages", "core-messages.yml" -> {
                     logger.warning("Module tried to get default config using name $name instead of ConfigName.MESSAGES")
                     MESSAGES
                 }
