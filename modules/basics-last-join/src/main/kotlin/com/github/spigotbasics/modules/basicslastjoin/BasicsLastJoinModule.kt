dd2bf10bcfca7ce5fb85347ad16fa77d4d455f5d
mfnalex <mfnalex@jeff-media.com>
2024-01-30 19:22:28 +0100

    prevent storage creation when module is not enabled

--- modules/basics-last-join/src/main/kotlin/com/github/spigotbasics/modules/basicslastjoin/BasicsLastJoinModule.kt
+++ modules/basics-last-join/src/main/kotlin/com/github/spigotbasics/modules/basicslastjoin/BasicsLastJoinModule.kt
@@ -2,42 +2,39 @@ package com.github.spigotbasics.modules.basicslastjoin
 
 import com.github.spigotbasics.core.module.AbstractBasicsModule
 import com.github.spigotbasics.core.module.ModuleInstantiationContext
+import com.github.spigotbasics.core.storage.NamespacedStorage
 import org.bukkit.event.player.PlayerJoinEvent
 
 class BasicsLastJoinModule(context: ModuleInstantiationContext) : AbstractBasicsModule(context) {
 
-    val storage = createStorage() // TODO: This shall not be possible before isEnabled is true.
-    //  Otherwise, the storage will be closed when the module is disabled and enabled again.
+    var storage: NamespacedStorage? = null
 
     override fun onEnable() {
+        storage = createStorage()
         eventBus.subscribe(PlayerJoinEvent::class.java) {
 
             val player = it.player
-            storage.getJsonObject(player.name) // Normaly use UUIDs. This is for testing purposes.
-                .whenComplete { json, u ->
+            storage?.getJsonObject(player.uniqueId.toString())?.whenComplete { json, u ->
 
-                    if (u != null) {
-                        logger.warning("Failed to get last join for ${it.player.name}")
-                        player.sendMessage("§cFailed to get last join. Please contact CMarco, he can fix this easily.")
-                        return@whenComplete
-                    }
-
-                    if (json != null) {
-                        val lastJoin = LastJoin.fromJson(json)
-                        player.sendMessage("Welcome back, ${it.player.name}! Your last join was @ ${lastJoin.dateTime}")
-                    } else {
-                        player.sendMessage("This is your first join, ${it.player.name}!")
-                    }
-                }.thenRun() {
-                    try {
-                        player.sendMessage("We'll now set your new last join to now")
-                        storage.setJsonObject(player.name, LastJoin().toJson())
-                        player.sendMessage("Setter created")
-                    } catch (e: Throwable) {
-                        player.sendMessage("§cSetter failed: ${e.message}")
-                        e.printStackTrace()
-                    }
+                if (u != null) {
+                    logger.warning("Failed to get last join for ${it.player.name}")
+                    player.sendMessage("§cFailed to get last join. Please contact CMarco, he can fix this easily.")
+                    return@whenComplete
                 }
+
+                if (json != null) {
+                    val lastJoin = LastJoin.fromJson(json)
+                    player.sendMessage("Welcome back, ${it.player.name}! Your last join was @ ${lastJoin.dateTime}")
+                } else {
+                    player.sendMessage("This is your first join, ${it.player.name}!")
+                }
+            }?.thenRun() {
+                try {
+                    storage?.setJsonObject(player.uniqueId.toString(), LastJoin().toJson())
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+            }
 
         }
     }
