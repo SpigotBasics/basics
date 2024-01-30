9138d26d60c369f2003aaf7140c606404e8273f9
mfnalex <mfnalex@jeff-media.com>
2024-01-30 09:48:05 +0100

    made createStorage internal, added todos

--- core/src/main/kotlin/com/github/spigotbasics/core/storage/StorageManager.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/storage/StorageManager.kt
@@ -4,6 +4,10 @@ import com.github.spigotbasics.core.config.ConfigName
 import com.github.spigotbasics.core.config.CoreConfigManager
 import com.github.spigotbasics.core.storage.backends.JsonBackend
 
+// TODO: Storages saved in vals will be closed when a module is disabled and then enabled again.
+// Possible fix: Prevent storages from being created before isEnabled is true.
+
+// TODO: Plugins shall be notified about player joins instead of listening on their own, so they can fetch playerdata if needed
 class StorageManager(configManager: CoreConfigManager) {
 
     val config = configManager.getConfig(
@@ -13,12 +17,12 @@ class StorageManager(configManager: CoreConfigManager) {
         StorageConfig::class.java
     )
 
-    private val backend: StorageBackend = when(config.storageType) {
+    private val backend: StorageBackend = when (config.storageType) {
         StorageType.JSON -> JsonBackend(config.jsonDirectory)
         StorageType.SQLITE -> TODO("SQLite Storage backend") //SqliteBackend(config.sqliteFile)
     }
 
-    fun createStorage(namespace: String): NamespacedStorage {
+    internal fun createStorage(namespace: String): NamespacedStorage {
         return NamespacedStorage(backend, namespace)
     }
 
