ca82e348dbc39b4e08e0064fc60c420246a38719
mfnalex <mfnalex@jeff-media.com>
2024-02-02 23:35:58 +0100

    added zipDistribution task, added mysql prefix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
+    base
     id("basics.kotlin-conventions")
     id("com.github.johnrengelman.shadow") apply false
 }
@@ -8,4 +11,20 @@ tasks.register("copyAllToTestServer") {
     description = "Copies the plugin and all modules to the test server"
     dependsOn("plugin:copyPluginToTestServer")
     dependsOn("modules:copyAllModulesToTestServer")
+}
+
+tasks.register<Zip>("zipDistribution") {
+    group = "basics"
+    description = "Bundle the plugin and all modules into a single zip file."
+
+    archiveFileName = "basics-$version.zip"
+    destinationDirectory = file("build/dist")
+
+    from(project(":plugin").tasks.getByName("shadowJar", ShadowJar::class).archiveFile)
+
+    for(module in project(":modules").subprojects) {
+        from(module.tasks.getByName("shadowJar", ShadowJar::class).archiveFile) {
+            into("Basics/modules")
+        }
+    }
 }
