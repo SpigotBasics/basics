8950d980347c5538848731ba812900c0a746ab14
mfnalex <mfnalex@jeff-media.com>
2024-02-03 19:52:41 +0100

    per world permissions

--- modules/basics-world/src/main/kotlin/com/github/spigotbasics/modules/basicsworld/BasicsWorldModule.kt
+++ modules/basics-world/src/main/kotlin/com/github/spigotbasics/modules/basicsworld/BasicsWorldModule.kt
@@ -5,6 +5,7 @@ import com.github.spigotbasics.core.module.AbstractBasicsModule
 import com.github.spigotbasics.core.module.loader.ModuleInstantiationContext
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.permissions.Permission
 
 class BasicsWorldModule(context: ModuleInstantiationContext) : AbstractBasicsModule(context) {
 
@@ -18,12 +19,18 @@ class BasicsWorldModule(context: ModuleInstantiationContext) : AbstractBasicsMod
     fun msgUnsuccessful(world: String) = messages.getMessage("teleport-unsuccessful").tagUnparsed("world", world)
     fun msgStartingTeleport(world: String) = messages.getMessage("starting-teleport").tagUnparsed("world", world)
 
+    val worldPermissions = mutableMapOf<String, Permission>()
+
     override fun onEnable() {
         createCommand("world", permission)
             .usage("/world <world>")
             .description("Teleport to another world")
             .executor(WorldCommand(this))
             .register()
+
+        server.worlds.forEach {
+            getWorldPermission(it.name)
+        }
     }
 
     override fun reloadConfig() {
@@ -31,7 +38,11 @@ class BasicsWorldModule(context: ModuleInstantiationContext) : AbstractBasicsMod
         messages.reload()
     }
 
-
+    fun getWorldPermission(name: String): Permission {
+        return worldPermissions.computeIfAbsent(name.lowercase()) {
+            permissionManager.createSimplePermission("basics.world.${name.lowercase()}", "Allows to switch to world '$name' using /world $name")
+        }
+    }
 
 
 }
