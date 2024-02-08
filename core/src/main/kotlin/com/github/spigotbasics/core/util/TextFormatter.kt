0967b05d80a878cf71aad21a7c999ce0dc53b178
mfnalex <mfnalex@jeff-media.com>
2024-02-08 17:10:23 +0100

    improved /enchant, added utilities

---
+++ core/src/main/kotlin/com/github/spigotbasics/core/util/TextFormatter.kt
@@ -0,0 +1,9 @@
+package com.github.spigotbasics.core.util
+
+object TextFormatter {
+
+    fun toHumanReadable(value: String): String {
+        return value.split("_").joinToString(" ") { word -> word.lowercase().replaceFirstChar { it.uppercase() } }
+    }
+
+}
