46ffd684eb45de7c2d12947e4c61e12d95924ef6
mfnalex <mfnalex@jeff-media.com>
2024-02-08 17:10:23 +0100

    improved /enchant, added utilities

--- modules/basics-enchant/src/main/kotlin/com/github/spigotbasics/modules/basicsenchant/BasicsEnchantModule.kt
+++ modules/basics-enchant/src/main/kotlin/com/github/spigotbasics/modules/basicsenchant/BasicsEnchantModule.kt
@@ -5,10 +5,13 @@ import com.github.spigotbasics.core.command.BasicsCommandExecutor
 import com.github.spigotbasics.core.command.CommandResult
 import com.github.spigotbasics.core.config.ConfigName
 import com.github.spigotbasics.core.extensions.partialMatches
+import com.github.spigotbasics.core.extensions.toHumanReadable
+import com.github.spigotbasics.core.extensions.toRoman
 import com.github.spigotbasics.core.module.AbstractBasicsModule
 import com.github.spigotbasics.core.module.loader.ModuleInstantiationContext
 import org.bukkit.Bukkit
 import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 class BasicsEnchantModule(context: ModuleInstantiationContext) : AbstractBasicsModule(context) {
@@ -22,11 +25,13 @@ class BasicsEnchantModule(context: ModuleInstantiationContext) : AbstractBasicsM
 
     val enchantments = Bukkit.getRegistry(Enchantment::class.java)?.map { it.key.key }?.toList() ?: emptyList()
 
-    fun msgEnchantedSelf(item: ItemStack, enchantment: Enchantment, level: Int)
-    = msg.getMessage("enchanted-self")
-        .tagParsed("item", item.type.name)
-        .tagParsed("enchantment", enchantment.key.key)
-        .tagParsed("level", level.toString())
+    fun msgEnchantedSelf(tag: EnchantOperationMessageTag) = msg.getMessage("enchanted-self").tags(tag)
+
+    fun msgEnchantedOthers(tag: EnchantOperationMessageTag, player: Player) = msg.getMessage("enchanted-others")
+        .tags(tag)
+        .concerns(player)
+
+    fun msgRemovedSelf(tag: EnchantOperationMessageTag) = msg.getMessage("removed-self").tags(tag)
 
     override fun onEnable() {
         createCommand("enchant", permission)
@@ -39,22 +44,25 @@ class BasicsEnchantModule(context: ModuleInstantiationContext) : AbstractBasicsM
     inner class EnchantExecutor : BasicsCommandExecutor(this@BasicsEnchantModule) {
         override fun execute(context: BasicsCommandContext): CommandResult {
             val player = requirePlayerOrMustSpecifyPlayerFromConsole(context.sender) // TODO: Create a requirePlayer(Sender) method
+            val args = context.args
             if(args.size == 0) return CommandResult.USAGE
             val item = requireItemInHand(player)
-            val args = context.args
-            val enchantment = getEnchantment(args[0])
-            if(enchantment == null) failInvalidArgument(args[0])
-            var level = (item.itemMeta?.getEnchantLevel(enchantment!!) ?: 0) + 1
+            val enchantment = getEnchantment(args[0]) ?: throw failInvalidArgument(args[0]).asException()
+            var level = (item.itemMeta?.getEnchantLevel(enchantment) ?: 0) + 1
             if(args.size > 1) {
                 level = args[1].toIntOrNull() ?: throw failInvalidArgument(args[1]).asException()
             }
+
+            val tag = EnchantOperationMessageTag(item, enchantment, level)
+
             if(level == 0) {
-                item.removeEnchantment(enchantment!!) // TODO: message "removed enchantment"
+                item.removeEnchantment(enchantment) // TODO: message "removed enchantment"
+                msgRemovedSelf(tag).sendToPlayer(player)
                 return CommandResult.SUCCESS
             }
 
-            item.addUnsafeEnchantment(enchantment!!, level) // TODO: Separate permission for unsafe enchants, separate permission for max-level and for enchantment type
-            msgEnchantedSelf(item, enchantment, level).sendToPlayer(player)
+            item.addUnsafeEnchantment(enchantment, level) // TODO: Separate permission for unsafe enchants, separate permission for max-level and for enchantment type
+            msgEnchantedSelf(tag).sendToPlayer(player)
             return CommandResult.SUCCESS
         }
 
