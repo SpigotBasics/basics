e71b3514a4635645f8e413b38dd8a36235b83937
mfnalex <mfnalex@jeff-media.com>
2024-02-08 03:43:27 +0100

    add requireItemInHand

--- core/src/main/kotlin/com/github/spigotbasics/core/command/BasicsCommandExecutor.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/command/BasicsCommandExecutor.kt
@@ -6,6 +6,7 @@ import com.github.spigotbasics.core.module.BasicsModule
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.bukkit.permissions.Permission
 
 abstract class BasicsCommandExecutor(module: BasicsModule) {
@@ -33,7 +34,7 @@ abstract class BasicsCommandExecutor(module: BasicsModule) {
     @Throws(BasicsCommandException::class)
     fun notFromConsole(sender: CommandSender): Player {
         if(sender !is Player) {
-            throw BasicsCommandException(CommandResult.notFromConsole())
+            throw CommandResult.NOT_FROM_CONSOLE.asException()
         }
         return sender
     }
@@ -42,7 +43,7 @@ abstract class BasicsCommandExecutor(module: BasicsModule) {
     fun requirePlayerOrMustSpecifyPlayerFromConsole(sender: CommandSender): Player {
         val player = sender as? Player
         if(player == null) {
-            throw BasicsCommandException(CommandResult.mustBePlayerOrSpecifyPlayerFromConsole())
+            throw CommandResult.MUST_BE_PLAYER_OR_SPECIFY_PLAYER_FROM_CONSOLE.asException()
         }
         return player
     }
@@ -50,18 +51,26 @@ abstract class BasicsCommandExecutor(module: BasicsModule) {
     @Throws(BasicsCommandException::class)
     fun failIfFlagsLeft(context: BasicsCommandContext) {
         if(context.flags.isEmpty()) return
-        throw BasicsCommandException(CommandResult.unknownFlag(context.flags[0]))
+        throw CommandResult.unknownFlag(context.flags[0]).asException()
     }
 
     @Throws(BasicsCommandException::class)
     fun failInvalidArgument(argument: String): CommandResult {
-        throw BasicsCommandException(CommandResult.invalidArgument(argument))
+        throw CommandResult.invalidArgument(argument).asException()
     }
 
     fun requirePermission(sender: CommandSender, permission: Permission) {
         if(!sender.hasPermission(permission)) {
-            throw BasicsCommandException(CommandResult.noPermission(permission))
+            throw CommandResult.noPermission(permission).asException()
         }
+    }
+
+    fun requireItemInHand(player: Player): ItemStack {
+        val item: ItemStack? = player.inventory.itemInMainHand
+        if(item == null || item.type.isAir) {
+            throw CommandResult.MUST_HOLD_ITEM_IN_HAND.asException()
+        }
+        return item
     }
 
 }
