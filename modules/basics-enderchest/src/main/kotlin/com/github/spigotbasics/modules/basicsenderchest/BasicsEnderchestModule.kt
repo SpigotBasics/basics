862000e62122001dcde83fa6cea051881f2f7329
mfnalex <mfnalex@jeff-media.com>
2024-02-10 15:08:39 +0100

    added enderchest module

---
+++ modules/basics-enderchest/src/main/kotlin/com/github/spigotbasics/modules/basicsenderchest/BasicsEnderchestModule.kt
@@ -0,0 +1,25 @@
+package com.github.spigotbasics.modules.basicsenderchest
+
+import com.github.spigotbasics.core.module.AbstractBasicsModule
+import com.github.spigotbasics.core.module.loader.ModuleInstantiationContext
+
+class BasicsEnderchestModule(context: ModuleInstantiationContext) : AbstractBasicsModule(context) {
+    val permission =
+        permissionManager.createSimplePermission("basics.enderchest", "Allows opening your enderchest with /enderchest")
+
+    val permissionOthers =
+        permissionManager.createSimplePermission(
+            "basics.enderchest.others",
+            "Allows opening other players enderchests with /enderchest <player>",
+        )
+
+    val inventoryTitle get() = messages.getMessage("inventory-title")
+
+    override fun onEnable() {
+        createCommand("enderchest", permission)
+            .description("Opens your enderchest")
+            .usage("[player]")
+            .executor(EnderchestCommand(this))
+            .register()
+    }
+}
