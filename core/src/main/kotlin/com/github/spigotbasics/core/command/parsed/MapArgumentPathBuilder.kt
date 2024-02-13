23071c508fac19e9228fc44967bccc2ff5ac56e2
mfnalex <mfnalex@jeff-media.com>
2024-02-13 19:16:17 +0100

    Moved common logic of GenericArgumentPathBuilder and MapArgumentPathBuilder into now abstract ArgumentPathBuilder

--- core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/MapArgumentPathBuilder.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/MapArgumentPathBuilder.kt
@@ -1,23 +1,6 @@
 package com.github.spigotbasics.core.command.parsed
 
-import org.bukkit.permissions.Permission
-
-class MapArgumentPathBuilder : ArgumentPathBuilder<MapCommandContext> {
-    private var senderType: SenderType<*> = AnySender
-    private var arguments = emptyList<Pair<String, CommandArgument<*>>>()
-    private var permissions = emptyList<Permission>()
-    // private var contextBuilder: ((Map<String, Any?>) -> T)? = null
-
-    override fun senderType(senderType: SenderType<*>) = apply { this.senderType = senderType }
-
-    override fun playerOnly() = apply { senderType(PlayerSender) }
-
-    override fun permissions(vararg permissions: Permission) = apply { this.permissions = permissions.toList() }
-
-    override fun arguments(vararg arguments: Pair<String, CommandArgument<*>>) = apply { this.arguments = arguments.toList() }
-
-    // fun contextBuilder(contextBuilder: (Map<String, Any?>) -> T) = apply { this.contextBuilder = contextBuilder }
-
+class MapArgumentPathBuilder : ArgumentPathBuilder<MapCommandContext>() {
     override fun build() =
         ArgumentPath(
             senderType,
