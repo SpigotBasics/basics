081949080793535843f97e64f0ee62e031a5465a
mfnalex <mfnalex@jeff-media.com>
2024-02-15 14:26:52 +0100

    added CommandSender to CommandArgument

--- core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/CommandArgument.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/CommandArgument.kt
@@ -2,11 +2,12 @@ package com.github.spigotbasics.core.command.parsed.arguments
 
 import com.github.spigotbasics.core.Basics
 import com.github.spigotbasics.core.messages.Message
+import org.bukkit.command.CommandSender
 
 abstract class CommandArgument<T>(open val name: String? = null) {
-    abstract fun parse(value: String): T?
+    abstract fun parse(sender: CommandSender, value: String): T?
 
-    open fun tabComplete(typing: String): List<String> = emptyList()
+    open fun tabComplete(sender: CommandSender, typing: String): List<String> = emptyList()
 
     // TODO: This is using the static Singleton :/
     open fun errorMessage(value: String? = null): Message = Basics.messages.invalidValueForArgument(getArgumentName(), value ?: "null")
