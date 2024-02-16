30b8dffd73ea079437d7686ac467a71294aadf74
mfnalex <mfnalex@jeff-media.com>
2024-02-16 19:27:55 +0100

    added SelectorMultiEntityArg and SelectorSingleEntityArg

---
+++ core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/SelectorMultiEntityArg.kt
@@ -0,0 +1,25 @@
+package com.github.spigotbasics.core.command.parsed.arguments
+
+import com.github.spigotbasics.core.messages.Message
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Entity
+import org.bukkit.entity.Player
+
+class SelectorMultiEntityArg(name: String) : SelectorEntityArgBase<List<Entity>>(name) {
+    override fun parse(
+        sender: CommandSender,
+        value: String,
+    ): List<Entity>? {
+        return get(sender, value, allowMultiple = true, allowEntities = true).fold(
+            { _ -> null },
+            { it },
+        )
+    }
+
+    override fun errorMessage(
+        sender: CommandSender,
+        value: String,
+    ): Message {
+        return errorMessage0(sender, value, allowMultiple = true, allowEntities = true)
+    }
+}
