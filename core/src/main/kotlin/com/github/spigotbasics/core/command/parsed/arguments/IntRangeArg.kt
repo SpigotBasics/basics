39712b8507bd5a9e40050b20bf245b6a9f00e3ff
mfnalex <mfnalex@jeff-media.com>
2024-02-17 23:28:49 +0100

    tabcomplete for IntRangeArg

--- core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/IntRangeArg.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/IntRangeArg.kt
@@ -1,6 +1,7 @@
 package com.github.spigotbasics.core.command.parsed.arguments
 
 import com.github.spigotbasics.core.Basics
+import com.github.spigotbasics.core.extensions.partialMatches
 import com.github.spigotbasics.core.messages.Message
 import org.bukkit.command.CommandSender
 
@@ -23,5 +24,12 @@ class IntRangeArg(name: String, private val min: () -> Int, private val max: () 
     ): Message {
         val given = value.toIntOrNull() ?: return Basics.messages.invalidValueForArgumentMustBeInteger(name, value)
         return Basics.messages.invalidValueForArgumentNumberNotInRange(name, given, min(), max())
+    }
+
+    override fun tabComplete(
+        sender: CommandSender,
+        typing: String,
+    ): List<String> {
+        return listOf(min().toString(), max().toString()).partialMatches(typing)
     }
 }
