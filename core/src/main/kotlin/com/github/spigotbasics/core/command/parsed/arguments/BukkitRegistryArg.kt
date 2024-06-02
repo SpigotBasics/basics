6bfa3d16e38d90767409d92078c3ead53a44082c
Miles Holder <mwholder2005@gmail.com>
2024-06-02 10:48:35 -0500

    Fix BukkitRegistryArg match overflow

--- core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/BukkitRegistryArg.kt
+++ core/src/main/kotlin/com/github/spigotbasics/core/command/parsed/arguments/BukkitRegistryArg.kt
@@ -1,11 +1,11 @@
 package com.github.spigotbasics.core.command.parsed.arguments
 
+import com.github.spigotbasics.core.extensions.partialMatches
 import org.bukkit.Bukkit
 import org.bukkit.Keyed
 import org.bukkit.NamespacedKey
 import org.bukkit.Registry
 import org.bukkit.command.CommandSender
-import java.util.stream.Collectors
 
 class BukkitRegistryArg<T : Keyed>(name: String, registryClass: Class<T>) : CommandArgument<T>(name) {
     private val registry: Registry<T> = Bukkit.getRegistry(registryClass) ?: error("$registryClass is not a bukkit registry type")
@@ -21,12 +21,12 @@ class BukkitRegistryArg<T : Keyed>(name: String, registryClass: Class<T>) : Comm
         sender: CommandSender,
         typing: String,
     ): List<String> {
-        return registry.stream().map { entry ->
+        return registry.map { entry ->
             val namespacedKeyed = entry.key
             if (namespacedKeyed.namespace == NamespacedKey.MINECRAFT) {
                 return@map namespacedKeyed.key
             }
             return@map namespacedKeyed.toString()
-        }.collect(Collectors.toList())
+        }.partialMatches(typing)
     }
 }
